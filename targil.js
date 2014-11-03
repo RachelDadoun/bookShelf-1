16c3fd63a067190bb9ad49173a517444ac0a6b23
RachelDadoun <RachelDadoun@users.noreply.github.com>
2014-11-03 20:33:27 +0200

    Update targil.js

--- targil.js
+++ targil.js
@@ -29,7 +29,7 @@ function searchIt(){
 	var searchResults = [];
 	var nameToSearchFor = document.getElementById("searchName").value;
 	for (var i=0;i<booksArray.length;i++) {
-		if (booksArray[i].bookName.indexOf(nameToSearchFor) > -1) {
+		if (booksArray[i].indexOf(nameToSearchFor) > -1) {
 			searchResults.push(booksArray[i]);	
 		}
 	}
