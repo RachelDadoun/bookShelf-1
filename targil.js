caeefe763b2f4fa8a1ad22240fff9425ef8ca9cb
RachelDadoun <RachelDadoun@users.noreply.github.com>
2014-11-04 13:16:04 +0200

    Update targil.js

--- targil.js
+++ targil.js
@@ -2,14 +2,13 @@ function Book (bookName, authorName, score) {
 	this.bookName = bookName;
 	this.authorName = authorName;
 	this.score = score;
+	this.id = "Li" + (seq++);
 };
 
-var currentBookName;
-var currentAuthorName;
-var currentScore;
-
 var booksArray = [];
-var tOut=true;
+var tOut;
+var seq = 0;
+
 function reset(e){
 	document.getElementById('bookName').value = "";
 	document.getElementById('authorName').value = "";
@@ -60,14 +59,12 @@ function addBook(){
 	var score = document.getElementById('score').value;
 	var book = new Book(bookName, authorName, score);
 	booksArray.push(book);
+	//clearList();
 	//buildListFromArray(booksArray);
 	addToList(book);
 	reset();
 }
 
-//function buildListFromArray(booksArray) {
-		
-//}
 
 function clearList(){
 	var ul = document.getElementById("bookList");	
@@ -75,9 +72,24 @@ function clearList(){
 }
 
 function removeItem(e) {
-	e.target.parentElement.remove();
+	
+	index = getArrIndByElement(e.target.parentElement);
+	booksArray.splice(index,1);
+	clearList();
+	buildListFromArray(booksArray);
 }
 
+function getArrIndByElement(element)
+{
+	eId = element.getAttribute("id");
+    for ( var i=0;i<booksArray.length;i++) 
+	{
+		if (booksArray[i].id == eId)
+		{
+			return i;
+		}
+	}
+}
 function submitEdit(e) {
 	/**if (e.keyCode == 13) {
 		e.target.parentElement.parentElement.children[3].style.display = "inline";
@@ -88,22 +100,26 @@ function submitEdit(e) {
 	if (e.keyCode == 27) {
 		e.target.parentElement.parentElement.children[3].innerHTML = "edit";
 		var li = e.target.parentElement.parentElement;
-		li.children[0].innerHTML = currentBookName;
-		li.children[1].innerHTML = currentAuthorName;
-		li.children[2].innerHTML = currentScore;
+		var ind = getArrIndByElement(li);
+		li.children[0].innerHTML = booksArray[ind].bookName;;
+		li.children[1].innerHTML = booksArray[ind].authorName;
+		li.children[2].innerHTML = booksArray[ind].score;
 	}
 }
 
 function editItem(e) {
 	if (e.target.innerHTML == "save") {
 		var li = e.target.parentElement;
+		var ind = getArrIndByElement(li);
 		e.target.innerHTML = "edit";
-		var bookName = li.children[0].children[0].value;
-		var authorName = li.children[1].children[0].value;
-		var score = li.children[2].children[0].value;
-		li.children[0].innerHTML = bookName;
-		li.children[1].innerHTML = authorName;
-		li.children[2].innerHTML = score;
+		booksArray[ind].bookName = li.children[0].children[0].value;
+		booksArray[ind].authorName = li.children[1].children[0].value;
+		booksArray[ind].score = li.children[2].children[0].value;
+		li.children[0].innerHTML = booksArray[ind].bookName;
+		li.children[1].innerHTML = booksArray[ind].authorName;
+		li.children[2].innerHTML = booksArray[ind].score;
+		
+		
 	} else {
 	var divWeWantToReplace = e.target.parentElement.children[0];
 	currentBookName = divWeWantToReplace.innerHTML;
@@ -152,6 +168,7 @@ function addToList(book) {
 		var edit = document.createElement("span");
 		edit.setAttribute("onclick", "editItem(event)");
 		edit.innerHTML = "edit";
+		newElement.setAttribute("id", book.id);
 		newElement.appendChild(bookNameDiv);
 		newElement.appendChild(authorNameDiv);
 		newElement.appendChild(scoreDiv);
@@ -163,5 +180,6 @@ function addToList(book) {
 
 function clearListAndArray(){
 	clearList();
+	seq=0;
 	booksArray = [];
 }
